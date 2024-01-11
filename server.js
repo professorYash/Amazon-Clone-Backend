3b54af792e7ab236191bba2dfe16b4576d75997f
Yash86400-Design <knowledge86400@gmail.com>
2024-01-11 22:31:40 +0530

    Added cors origin of deployed frontend

--- server.js
+++ server.js
@@ -13,7 +13,10 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: "https://amazon-clone-frontend-two.vercel.app/",
+  allowedHeaders: ["GET", "PUT", "PATCH"]
+}));
 
 connectDB();
 
